f0c89c83b5e4e2e5a5aa4a413619a4ff3acb303c
Adam <adamcolyer@gmail.com>
2025-09-09 11:47:03 -0500

    to the moon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 function Header() {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <Navbar bg="dark" variant="dark" expand="md" sticky="top" data-bs-theme="dark" role="navigation" className="py-1">
+    <Navbar
+      bg="dark"
+      variant="dark"
+      expand="md"
+      data-bs-theme="dark"
+      role="navigation"
+      className="nav py-1"
+      expanded={expanded}
+      onToggle={setExpanded}
+    >
       <Container className="py-1">
         <Navbar.Toggle aria-controls="main-navbar-nav" />
         <Navbar.Collapse id="main-navbar-nav">
           <Nav className="mx-auto justify-content-center py-0" as="ul">
             <Nav.Item as="li">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/" onClick={() => setExpanded(false)}>Home</Nav.Link>
             </Nav.Item>
             <Nav.Item as="li">
-              <Nav.Link as={Link} to="/about">About</Nav.Link>
+              <Nav.Link as={Link} to="/about" onClick={() => setExpanded(false)}>About</Nav.Link>
             </Nav.Item>
             <Nav.Item as="li">
-              <Nav.Link as={Link} to="/services">Services</Nav.Link>
+              <Nav.Link as={Link} to="/services" onClick={() => setExpanded(false)}>Services</Nav.Link>
             </Nav.Item>
             <Nav.Item as="li">
-              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+              <Nav.Link as={Link} to="/contact" onClick={() => setExpanded(false)}>Contact</Nav.Link>
             </Nav.Item>
             <Nav.Item as="li">
-              <Nav.Link as={Link} to="/online-form">Online Form</Nav.Link>
+              <Nav.Link as={Link} to="/online-form" onClick={() => setExpanded(false)}>Online Form</Nav.Link>
             </Nav.Item>
           </Nav>
         </Navbar.Collapse>
