cc5424b6a232c965962005bfba6e870670459c30
Adam <adamcolyer@gmail.com>
2025-09-09 12:09:52 -0500

    send it

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../App.css';
 
 function Services() {
@@ -18,7 +19,7 @@ function Services() {
         <div className="card card-padded">
           <h3 className="text-center">Wills</h3>
           <p>A will is a legal document specifying how a person's property and assets should be handled after death. A testator (the person making the will) can give instructions on how the property should be divided, who should receive what portions or specific items, and who will take care of any surviving minor children. A will can establish a trust or make gifts to charity. Without a will the government determines how property will be distributed, and may impose a substantial tax burden on the estate. Wills must meet a state's legal requirements to be effective, so professional guidance from a qualified will and estate attorney is important.</p>
-          <p><a href="#">Wills: Why You Should Have One and the Lawyer's Role in Its Preparation</a></p>
+          <p><a href="https://www.americanbar.org/groups/real_property_trust_estate/resources/estate-planning/">Wills: Why You Should Have One and the Lawyer's Role in Its Preparation</a></p>
         </div>
         <div className="card card-padded">
           <h3 className="text-center">Trusts</h3>
@@ -36,7 +37,7 @@ function Services() {
           <h3 className="text-center">Estate Taxes</h3>
           <p>Many state and federal tax regulations impact estate planning. However, a carefully crafted estate plan can reduce the tax burden on an estate and estate beneficiaries. Both state and federal estate tax rules and regulations are extremely complex. The advice of an estate planning attorney to maximize estate tax savings is highly recommended.</p>
           <p>As of 2022, estates worth more than $3,000,000 must file a Minnesota Estate Tax Return, Form M706, and estates worth more than $11,700,000 must file a Federal Estate Tax Return, Form 706. Federal Gift Tax Law regulates gifts to individuals during life. The gift tax rules prevent people from giving away their assets to avoid estate taxes at death. Gifts exceeding $17,000 per recipient per calendar year require the giver of the gift to file a Federal Gift Tax Return.</p>
-          <p>Current Minnesota law allows only $3,000,000 to pass estate tax-free. Current federal law allows $12,920,000 to pass estate tax-free. The surviving spouse may inherit an unlimited amount of assets without paying estate taxes. However, if the taxable estate exceeds the above referenced dollar amounts and the entire estate is left to someone other than a spouse, the surviving spouse may lose the option of subsequently leaving an equivalent amount of assets to his or her chosen beneficiaries without incurring estate taxes. Estate planning attorneys can assist clients to create trusts that allow married couples to take full advantage of these shifting exemptions. <a href="#">Contact us</a> to find out how a qualified estate planning attorney can help.</p>
+          <p>Current Minnesota law allows only $3,000,000 to pass estate tax-free. Current federal law allows $12,920,000 to pass estate tax-free. The surviving spouse may inherit an unlimited amount of assets without paying estate taxes. However, if the taxable estate exceeds the above referenced dollar amounts and the entire estate is left to someone other than a spouse, the surviving spouse may lose the option of subsequently leaving an equivalent amount of assets to his or her chosen beneficiaries without incurring estate taxes. Estate planning attorneys can assist clients to create trusts that allow married couples to take full advantage of these shifting exemptions.<Link to="/contact">Contact us</Link>to find out how a qualified estate planning attorney can help.</p>
         </div>
       </section>
     </main>
